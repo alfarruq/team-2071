86e192d9ec70d7c836e29ef1c90ea620ade42879
alfarruq <mengboyevfarrux01@gmail.com>
2025-11-01 15:31:15 +0500

    changed section two css

--- src/components/SectionTwo/index.jsx
+++ src/components/SectionTwo/index.jsx
@@ -8,8 +8,8 @@ import four from '../../assets/four.png'
 
 function SectionTwo() {
     return (
-        <div>
-           <div className="left">
+        <div className='div' >
+           <div className="left div">
              <h2>BIZ HAQIMIZDA</h2>
             <h1>OFIS VA UYINGIZNI TEZ VA OSON TOZALASH</h1>
             <p>Amet minim mollit non deserunt ullamco  sit aliqua dolor do consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco aliqua dolo ame.</p>
@@ -17,7 +17,7 @@ function SectionTwo() {
             <p>Amet minim mollit non sit aliqua dolor do consequat sunt nostrud amet. Amet mollit ullamco aliqua dolo ame.</p>
             <button>Bizning Xizmatlarimiz</button>
            </div>
-           <div className="right">
+           <div className="right div">
             <img src={one} alt="" />
             <img src={two} alt="" />
             <img src={three} alt="" />
