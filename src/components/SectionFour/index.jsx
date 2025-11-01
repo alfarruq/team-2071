43e3c1a0522439f49d873e08e0cf9039f53f7382
bilolbaxtiyorov <bilolbahtiyorov57@gmail.com>
2025-11-01 03:54:52 -0700

    bilil

--- src/components/SectionFour/index.jsx
+++ src/components/SectionFour/index.jsx
@@ -1,13 +1,28 @@
 
 
 import './style.css'
+import rasm from "../../assets/rasm1.png"
+import rasm1 from "../../assets/888.png"
+import rasm2 from "../../assets/888.png"
+
 
 function SectionFour() {
-    return ( 
+    return (
         <div>
-            <h2>SectionFour</h2>
+
+            <h3 className='h3'  >YANGI ISHLAR</h3>
+
+            <div className='bilol'>
+                <img src={rasm} alt="" />
+                <img src={rasm1} alt="" />
+                <img src={rasm2} alt="" />
+
+
+            </div>
         </div>
-     );
+
+
+    );
 }
 
 export default SectionFour;
