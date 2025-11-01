eaf602c1fff08c15deade3bd728f5e7a8592efb8
yusupovasilbek174-sys <yusupovasilbek174@gmail.com>
2025-11-01 15:34:55 +0500

    section one

--- src/components/SectionOne/index.jsx
+++ src/components/SectionOne/index.jsx
@@ -2,11 +2,36 @@
 
 import './style.css'
 
+import img from '../../assets/opa.png'
+
+
 function SectionOne() {
     return ( 
-        <div>
-            <h2>SectionOne</h2>
-        </div>
+        <div className='container' >
+            <div class="hero">
+
+                <div class="leftt"> 
+                  <p style={{marginTop: "150px"}}>YUQORI DARAJALI PROFESSIONAL TOZALASH</p>
+                  <br />
+                  <h1> <span>OSON TOZALANADIGAN</span> UY VA OFIS</h1>
+                  <br />
+                    
+                  <p> Amet minim mollit non deserunt ullamco  sit aliqua dolor do amet sint. Velit officia enim velit mollit. Exercitation veniam consequat sunt nostrud amet. </p>
+                  <br />
+                  <br />
+                    <button>Narx  oling</button>
+                </div>
+
+                <div class="rightt">
+                    <img src={img} alt="" />
+                </div>
+              </div>
+
+       
+        </div>   
+        
+        
+       
      );
 }
 
